83c0e0b833ea0b4d4ad9634a431ece80358a546f
Andrii Kushch <andrii.kushch@gmail.com>
2019-08-08 22:30:09 +0200

    implement spline

--- src/main.cpp
+++ src/main.cpp
@@ -58,6 +58,10 @@ int main() {
     // "42" at the start of the message means there's a websocket message event.
     // The 4 signifies a websocket message
     // The 2 signifies a websocket event
+
+    int lane = 1;
+    double distance_for_a_next_waypoint = 30;
+
     if (length && length > 2 && data[0] == '4' && data[1] == '2') {
 
       auto s = hasData(data);
@@ -91,50 +95,109 @@ int main() {
 
           json msgJson;
 
-          vector<double> next_x_vals;
-          vector<double> next_y_vals;
-
           /**
            * TODO: define a path made up of (x,y) points that the car will visit
            *   sequentially every .02 seconds
            */
 
-          // reuse of previous path
-          int previous_path_size = previous_path_x.size();
+          vector<double> ptsx;
+          vector<double> ptsy;
 
-          // real world coordinates x and y
-          double pos_x;
-          double pos_y;
-          double angle;
+          double ref_x = car_x;
+          double ref_y = car_y;
+          double ref_yaw = deg2rad(car_yaw);
+          double ref_vel = car_speed;
 
-          for (int i = 0; i < previous_path_size; ++i) {
+          int prev_size = previous_path_x.size();
+
+          if (prev_size < 2) {
+            double prev_car_x = car_x - cos(car_yaw);
+            double prev_car_y = car_y - sin(car_yaw);
+
+            ptsx.push_back(prev_car_x);
+            ptsx.push_back(car_x);
+
+            ptsy.push_back(prev_car_y);
+            ptsy.push_back(car_y);
+          } else {
+            ref_x = previous_path_x[prev_size-1];
+            ref_y = previous_path_y[prev_size-1];
+
+            double ref_x_prev = previous_path_x[prev_size-2];
+            double ref_y_prev = previous_path_y[prev_size-2];
+            ref_yaw = atan2(ref_y-ref_y_prev, ref_x-ref_x_prev);
+
+            ptsx.push_back(ref_x_prev);
+            ptsx.push_back(ref_x);
+
+            ptsy.push_back(ref_y_prev);
+            ptsy.push_back(ref_y);
+          }
+
+          // create a new point in Frenet coordinates
+
+          vector<double> next_wp0 = getXY(car_s + 1*distance_for_a_next_waypoint, (4+2*lane), map_waypoints_s, map_waypoints_x, map_waypoints_y);
+          vector<double> next_wp1 = getXY(car_s + 2*distance_for_a_next_waypoint, (4+2*lane), map_waypoints_s, map_waypoints_x, map_waypoints_y);
+          vector<double> next_wp2 = getXY(car_s + 3*distance_for_a_next_waypoint, (4+2*lane), map_waypoints_s, map_waypoints_x, map_waypoints_y);
+
+          ptsx.push_back(next_wp0[0]);
+          ptsx.push_back(next_wp1[0]);
+          ptsx.push_back(next_wp2[0]);
+
+          ptsy.push_back(next_wp0[1]);
+          ptsy.push_back(next_wp1[1]);
+          ptsy.push_back(next_wp2[1]);
+
+          // transform to local coordinates
+          for (int i = 0; i < ptsx.size(); i++) {
+            double shift_x = ptsx[i] - ref_x;
+            double shift_y = ptsy[i] - ref_y;
+
+            ptsx[i] = shift_x*cos(0-ref_yaw) - shift_y*sin(0-ref_yaw);
+            ptsy[i] = shift_x*sin(0-ref_yaw) + shift_y*cos(0-ref_yaw);
+          }
+
+          // use spline to generate smooth path
+          tk::spline s;
+          s.set_points(ptsx, ptsy);
+
+          // real planer points
+          vector<double> next_x_vals;
+          vector<double> next_y_vals;
+
+          // reuse previous path points
+          for (int i = 0; i < prev_size; i++) {
             next_x_vals.push_back(previous_path_x[i]);
             next_y_vals.push_back(previous_path_y[i]);
           }
 
-          if (previous_path_size == 0) {
-            pos_x = car_x;
-            pos_y = car_y;
-            angle = deg2rad(car_yaw);
-          } else {
-            pos_x = previous_path_x[previous_path_size-1];
-            pos_y = previous_path_y[previous_path_size-1];
+          double target_x = 30.0;
+          double target_y = s(target_x);
+          double target_dist = sqrt((target_x*target_x) - (target_y*target_y));
 
-            double pos_x2 = previous_path_x[previous_path_size-2];
-            double pos_y2 = previous_path_y[previous_path_size-2];
-            angle = atan2(pos_y-pos_y2,pos_x-pos_x2);
-          }
+          double x_add_on = 0;
 
-          vector<double> sd = getFrenet(pos_x, pos_y, angle, map_waypoints_x, map_waypoints_y);
+          // number of intervals for the target distance
+          double N = (target_dist/(.02*ref_vel/2.24));
 
-          double dist_inc = 0.4;
-          for (int i = 0; i < 50-previous_path_size; ++i) {
-            double next_s = sd[0] + (i+1) * dist_inc;
-            double next_d = 6;
-            vector<double> xy = getXY(next_s, next_d, map_waypoints_s, map_waypoints_x, map_waypoints_y);
+          for (int i = 0; i <= 50 - prev_size; i++) {
 
-            next_x_vals.push_back(xy[0]);
-            next_y_vals.push_back(xy[1]);
+            double x_point = x_add_on + target_x/N;
+            double y_point = s(x_point);
+
+            x_add_on = x_point;
+
+            double x_ref = x_point;
+            double y_ref = y_point;
+            // convert to global coordinates
+            x_point = x_ref*cos(ref_yaw)-y_ref*sin(ref_yaw);
+            y_point = x_ref*sin(ref_yaw)+y_ref*cos(ref_yaw);
+
+            x_point -= ref_x;
+            y_point -= ref_y;
+
+            next_x_vals.push_back(x_point);
+            next_y_vals.push_back(y_point);
           }
 
           msgJson["next_x"] = next_x_vals;
