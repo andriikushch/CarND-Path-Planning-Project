48676e284fae238e64a4158a1d7cef6f4b3cd113
Andrii Kushch <andrii.kushch@gmail.com>
2019-08-08 22:58:17 +0200

    smooth moving

--- src/main.cpp
+++ src/main.cpp
@@ -193,8 +193,8 @@ int main() {
             x_point = x_ref*cos(ref_yaw)-y_ref*sin(ref_yaw);
             y_point = x_ref*sin(ref_yaw)+y_ref*cos(ref_yaw);
 
-            x_point -= ref_x;
-            y_point -= ref_y;
+            x_point += ref_x;
+            y_point += ref_y;
 
             next_x_vals.push_back(x_point);
             next_y_vals.push_back(y_point);
